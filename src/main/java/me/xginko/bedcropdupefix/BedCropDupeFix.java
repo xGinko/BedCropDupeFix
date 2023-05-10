6c8e0a0c20ec2a8a80fcb78193cd35be08aa78ac
xGinko <rachel.audrey@mail.de>
2023-05-10 11:20:46 +0200

    fix bypass on 1.16

--- src/main/java/me/xginko/bedcropdupefix/BedCropDupeFix.java
+++ src/main/java/me/xginko/bedcropdupefix/BedCropDupeFix.java
@@ -3,13 +3,13 @@ package me.xginko.bedcropdupefix;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -26,8 +26,8 @@ public final class BedCropDupeFix extends JavaPlugin implements Listener {
     private void onPlayerInteract(PlayerInteractEvent event) {
         if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
 
-        PlayerInventory inventory = event.getPlayer().getInventory();
-        ItemStack heldItem = inventory.getItem(inventory.getHeldItemSlot());
+        Player player = event.getPlayer();
+        ItemStack heldItem = player.getInventory().getItem(player.getInventory().getHeldItemSlot());
         if (heldItem == null) return;
 
         // Check if player is holding a bed
@@ -37,7 +37,11 @@ public final class BedCropDupeFix extends JavaPlugin implements Listener {
         Block clickedBlock = event.getClickedBlock();
         if (clickedBlock == null) return; // Shouldn't be null since we're making sure the action is right click block but just in case
 
-        for (Block block : getAdjacentBlocks(clickedBlock.getLocation(), event.getBlockFace())) {
+        HashSet<Block> blocksToSearch = new HashSet<>();
+        blocksToSearch.addAll(getAdjacentBlocks(clickedBlock.getLocation(), event.getBlockFace()));
+        blocksToSearch.addAll(getAdjacentBlocks(clickedBlock.getLocation().add(0,0,1), getYawBlockFace(player))); // Search adjacent to top-side of bed
+
+        for (Block block : blocksToSearch) {
             String blockTypeName = block.getType().name();
             if (
                     blockTypeName.equals("WHEAT") || blockTypeName.equals("POTATOES")
@@ -46,7 +50,7 @@ public final class BedCropDupeFix extends JavaPlugin implements Listener {
                     || blockTypeName.equals("BEETROOTS")
             ) {
                 event.setCancelled(true);
-                getLogger().info("Player " + event.getPlayer().getName() + " failed duplicating crops with beds.");
+                getLogger().info("Player " + player.getName() + " failed duplicating crops with beds.");
                 return;
             }
         }
@@ -54,16 +58,13 @@ public final class BedCropDupeFix extends JavaPlugin implements Listener {
 
     private HashSet<Block> getAdjacentBlocks(Location clickedBlockLocation, BlockFace blockFace) {
         HashSet<Block> adjacentBlocks = new HashSet<>();
-
         Location searchLocation = clickedBlockLocation.add(getDirection(blockFace));
-
         adjacentBlocks.add(searchLocation.add(0, -1, 0).getBlock()); // Down
         adjacentBlocks.add(searchLocation.add(0, 2, 0).getBlock()); // Up
         adjacentBlocks.add(searchLocation.add(-1, -1, 0).getBlock()); // Left
         adjacentBlocks.add(searchLocation.add(2, 0, 0).getBlock()); // Right
         adjacentBlocks.add(searchLocation.add(-1, 0, 1).getBlock()); // Forward
         adjacentBlocks.add(searchLocation.add(0, 0, -2).getBlock()); // Downward
-
         return adjacentBlocks;
     }
 
@@ -76,5 +77,24 @@ public final class BedCropDupeFix extends JavaPlugin implements Listener {
             direction.normalize();
         }
         return direction;
+    }
+
+    private BlockFace getYawBlockFace(Player player) {
+        float yaw = player.getLocation().getYaw();
+        double rotation = (yaw - 90) % 360;
+        if (rotation < 0) {
+            rotation += 360.0;
+        }
+        if (0 <= rotation && rotation < 45) {
+            return BlockFace.WEST;
+        } else if (45 <= rotation && rotation < 135) {
+            return BlockFace.NORTH;
+        } else if (135 <= rotation && rotation < 225) {
+            return BlockFace.EAST;
+        } else if (225 <= rotation && rotation < 315) {
+            return BlockFace.SOUTH;
+        } else {
+            return BlockFace.WEST;
+        }
     }
 }
